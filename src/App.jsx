47411bc98e6c8a6cdd51b3dd94ddd89017f7471d
Daniel-spec-dell <fvgdanil@gmail.com>
2025-08-17 20:22:24 +0300

    add task solution ver 1.01

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,6 @@ export const posts = postsFromServer.map(post => ({
   comments: getPostbyId(post.id),
 }));
 
-console.log(posts);
-
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
